a5f8575df4c348808664f07d0187ef756cd77351
Roberto Sarati <roberto.sarati@live.com>
2024-01-31 00:26:44 +0100

    fix config

--- main.py
+++ main.py
@@ -5,36 +5,14 @@ import logging.config
 import pathlib
 import jsonLogger
 
-logging_config = {
-    "version": 1,
-    "disable_existing_loggers": False,
-    "formatters": {
-        "simple": {
-            "format": "%(levelname)s: %(message)s"
-        }
-    },
-    "handlers": {
-        "stdout": {
-            "class": "logging.StreamHandler",
-            "formatter": "simple",
-            "stream" : "ext://sys.stdout"
-        }
-    },
-    "loggers": {
-        "root": {
-            "level": "DEBUG",
-            "hanlders": ["stdout"]
-        }
-    }
-}
-logger = logging.getLogger("my_app")
+
 
 def setup_logging():
     config_file = pathlib.Path("logging.config.json")
     with open(config_file) as f_in:
         config = json.load(f_in)
 
-    # logging.config.dictConfig(config)
+    logging.config.dictConfig(config)
     # queue_handler = logging.getHandlerByName("queue_handler")
     # if queue_handler is not None:
     #     queue_handler.listener.start()
@@ -43,4 +21,6 @@ def setup_logging():
 
 if __name__ == '__main__':
     setup_logging()
+    logger = logging.getLogger()
+    logger.error("Ciao")
     logger.error("Ciaone", extra={"something_else": 14})
