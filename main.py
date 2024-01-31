5f47ac1c4675e83053e5cc3d15fba308796ea29e
Roberto Sarati <roberto.sarati@dxtcommodities.com>
2024-01-31 18:49:00 +0100

    graphana

--- main.py
+++ main.py
@@ -3,8 +3,11 @@ import json
 import logging
 import logging.config
 import pathlib
-import jsonLogger
 
+# example of usage grafana/loki api when you need push any log/message from your python scipt
+import requests
+import json
+import datetime
 
 
 def setup_logging():
@@ -13,14 +16,14 @@ def setup_logging():
         config = json.load(f_in)
 
     logging.config.dictConfig(config)
-    # queue_handler = logging.getHandlerByName("queue_handler")
-    # if queue_handler is not None:
-    #     queue_handler.listener.start()
-    #     atexit.register(queue_handler.listener.stop)
+    queue_handler = logging.getHandlerByName("queue_handler")
+    if queue_handler is not None:
+        queue_handler.listener.start()
+        atexit.register(queue_handler.listener.stop)
 
 
 if __name__ == '__main__':
     setup_logging()
     logger = logging.getLogger()
-    logger.error("Ciao")
-    logger.error("Ciaone", extra={"something_else": 14})
+    # logger.error("Ciao")
+    logger.error("Ciaone666", extra={"something_else": 14})
